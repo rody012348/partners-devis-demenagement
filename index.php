8298382b6e112ec4e91314fc250aedb22d6d13fd
rody012348 <84115485+rody012348@users.noreply.github.com>
2021-05-13 06:31:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="FR">																																
+	<head>
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!=='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-NMC4LMM');</script>
+		
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">			
+		<title>partners-devis-demenagement.ch | CH - GE - FR - ZH</title>																								
+		<meta name="description" content="Demandes Devis Offres Prix Déménagement Livraison Services Débarras Genève Suisse Annemasse Rhône Alpes 0779601029 https://partners-devis-demenagement.ch">
+		<meta property="og:title" content="partners-devis-demenagement.ch">
+		<meta property="og:title" content="0779601029 Devis offres Déménagements Débarras d'Appartements Maisons Genève Lausanne Livraison Pianos">
+		<meta name="google-site-verification" content="zzeelZdkLYR6y9QvwZW6gTDgLbARBEFdn6BOoPtSdnU" />
+		<meta name="google-site-verification" content="2AM_36d6DIY9GYf0wwtZQiFsqtr_vRP1XAsRWbcj_nw" />
+		
+		<link rel="canonical" href="https://partners-devis-demenagement.ch/" />											
+		<link rel="stylesheet" href="dem.css" />
+		<link rel="icon" href="favicon.ico" />
+																								
+	</head>    			
+
+	<body>
+
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMC4LMM"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			
+	<header>
+
+	<nav>
+		
+		<h1>PARTNERS - des devis déménagements et autres Services<em>"COMPARATIFS"</em></h1> 																	
+		<h2><a href="https://docs.google.com/forms/d/15hVE3-SNalrmykKLx_SYVc3Ay1x9Hn9TJx2DyjY5mdM/edit" title="Envoyer votre demande d'offre !" >Vous déménagez ? Faite votre choix de devis ici, ou nous contacter par téléphone ou courriel</a></h2>
+
+	</nav>
+
+	</header>
+
+	<section>
+		
+		<h3>
+			
+		- Ce site a pour fonction de vous faciliter la vie, nous vous mettons en relation avec des entreprises de déménagements.
+		Locales pour mieux gérer votre temps et votre budget,<br />- Vous pouvez demander simplement des devis sans engagements, notre équipe se fera le plaisir d'analyser et vous envoyer sous 24 - 48 h heures les offres correspondantes à votre demande.<br />
+		- <em><u>Nous avons tissé des liens solides avec des entreprises du secteur</u></em>, le déménagement peut souvent être un facteur de stress pour cette raison nous nous engageons à vous mettre en relation avec des<br />
+		- Entreprises expérimentées dans le domaine des transports. Veuillez nous contacter si vous voulez connaitre le prix pour un déménagement ou service.
+
+		</h3>
+			
+		<br />
+		
+		<h4>+41 077 960 10 29<br />informations et tarifs - whatsapp - telegram <strong><br />quotation.partners@gmail.com</strong></h4>
+
+		<h5>
+
+		TYPE D'OFFRES DISPONIBLES :
+
+		</h5>
+				
+		<ul>
+		
+		<li><strong> Déménagements ÉCONOMIQUE</strong></li>
+		<li><strong> Déménagements STANDARD avec cartons</strong></li>
+		<li><strong> Déménagements CONFORT et CONFORT +</strong></li>
+		<li><strong> Devis Débarras d'appartements ou villas</strong></li>
+		<li><strong> Déménagement de Pianos + Manutention Lourdes</strong></li>
+		<li><strong> Déménagements Local et vers l'Europe</strong></li>
+		<li><strong> Nettoyages Services et Conciergeries</strong></li>
+		<li><strong> Réception des offres sous les meilleurs délais 24 - 48 h</strong></li>
+		<li><strong> TARIF HORAIRE sur demande pour déménagements et services</strong></li>
+		<li><strong> Nous vous garantissons des devis d'entreprises qualifiés.</strong></li>
+
+		</ul>
+
+		<div>
+
+		<p> - Consulter les offres disponibles > <a href="index2.php">Choisir votre type de service ou prestation de déménagement</a> </p>
+				
+		<p>En cas de questions relatives, aux demandes, nous sommes à votre disposition.<br />Pour vous aider
+		   Avec nos différents conseils personnalisés en ligne<br />
+		</p>
+				
+		</div>
+
+		<div> 		
+
+		<p> <a href="https://docs.google.com/forms/d/15hVE3-SNalrmykKLx_SYVc3Ay1x9Hn9TJx2DyjY5mdM/edit">Veuillez nous envoyer votre choix de prestation et demande.</a> </p><br />
+		<p><strong>- Madames Monsieurs, un déménagement un service ? Cela s'organise !</strong></p>
+
+		</div>
+
+		<br />
+
+	</section>
+
+	<footer>
+
+		<span><strong>Genève - Lausanne - Suisse - CH</strong></span><br />
+		<span><strong>Rhône Alpes - Haute-Savoie - FR</strong></span>
+		<div><a href="tel:+41779601029">CONTACTER</a></div>
+		<div>+41 077 960 10 29</div>
+		<a href="https://docs.google.com/forms/d/15hVE3-SNalrmykKLx_SYVc3Ay1x9Hn9TJx2DyjY5mdM/edit">quotation.partners@gmail.com</a><br />
+		<a class="copyright">copyright partners services devis déménagements transports 2021 tous droits réservés</a><br />
+
+	</footer>
+
+	</body>																															
+</html>
---
+++ index2.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="FR">
+	<head>
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-NMC4LMM');</script>
+	
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+		<title>partners-devis-demenagement.ch | CH - GE - FR - ZH</title>
+		<meta name="description" content="Demandes Devis Offres Prix Déménagement Livraison Services Débarras Genève Suisse Annemasse Rhône Alpes 0779601029 https://partners-devis-demenagement.ch">
+		<meta property="og:title" content="partners-devis-demenagement.ch">
+		<meta property="og:title" content="Devis offres Déménagements Débarras d'Appartements Maisons Genève Lausanne Livraison Pianos 0779601029">
+		<meta name="google-site-verification" content="zzeelZdkLYR6y9QvwZW6gTDgLbARBEFdn6BOoPtSdnU" />
+		<meta name="google-site-verification" content="2AM_36d6DIY9GYf0wwtZQiFsqtr_vRP1XAsRWbcj_nw" />
+		
+		<link rel="stylesheet" href="dem2.css" />
+		<link rel="icon" href="favicon.ico" />
+
+	</head>
+	
+	<body>
+
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMC4LMM"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+	<header>
+
+	<nav>
+		
+		<h1>OFFRES - DEVIS prise en charge pour votre déménagement ou services</h1>
+								
+	</nav>
+
+	</header>
+		
+	<section>
+
+		<h3><a href="https://docs.google.com/forms/d/15hVE3-SNalrmykKLx_SYVc3Ay1x9Hn9TJx2DyjY5mdM/edit" title="Envoyer votre demande d'offre !" >Choisissez votre formule de devis et nous contacter par téléphone ou courriel.</a></h3>
+
+		<div> -
+			Sélectionner les prestations que vous souhaitez recevoir :<br />
+			- Temps pour recevoir une offre en moyenne 24 - 48 h<br />
+			<a href="tel:+41779601029">CONTACTER</a><br />
+			<a href="mailto:quotation.partners@gmail.com">PAR COURRIEL</a><br />
+		</div>
+
+        <br />
+
+		<form action="mailto:quotation.partners@gmail.com" method="POST" > 
+
+		<input type="checkbox" name="econome[]" id="econome" /> <label for="econome">
+			   ÉCONOME : <br />
+			   (pas de fournitures de cartons) - vous préparez et emballer les cartons et objets personnels ("DEVIS sans démontage et remontage des meubles")</label><br />
+
+		<br />
+
+        <input type="checkbox" name="standard[]" id="standard" /> <label for="standard">
+			   STANDARD : <br />
+			   (inclus les fournitures des cartons) - vous préparez et emballer les cartons et objets personnels ("DEVIS avec démontage et remontage des meubles")</label><br />
+
+		<br />
+
+        <input type="checkbox" name="confort[]" id="confort" /> <label for="confort">
+			   CONFORT et CONFORT +: <br />
+			   (inclus les fournitures des cartons) - nous préparerons et emballons vos cartons et objets personnels ("DEVIS avec démontage et remontage des meubles")</label><br />
+
+		<br />
+
+        <input type="checkbox" name="tarifhoraire[]" id="tarifhoraire" /> <label for="tarifhoraire">
+			   TARIF HORAIRE : <br />
+			   (pas de fournitures de cartons) - vous préparez et emballer les cartons et objets personnels ("DEVIS sans démontage et remontage des meubles")</label><br />
+
+		<br />
+
+        <input type="checkbox" name="pianodroit[]" id="pianodroit" /> <label for="pianodroit">
+			   PIANO DROIT : <br />
+			   ("déménagements de pianos droits")</label><br />
+
+		<br />
+
+		<input type="checkbox" name="pianoqueue[]" id="pianoqueue" /> <label for="pianoqueue">
+			   PIANO A QUEUE : <br />
+			   ("déménagements de pianos à queue")</label><br />
+
+		<br />
+
+		<input type="checkbox" name="debarras[]" id="debarras" /> <label for="debarras">
+			   DÉBARRAS : <br />
+			   ("débarras d'appartements et maisons")</label><br /><br />
+		
+		
+		<textarea name="message[]" cols="35" rows="7" ></textarea>
+ 		<br>
+		Faites ici votre demande devis: <br>
+
+		<input type="submit" value="envoyer" title="Envoyer nous votre demande d'offre" />
+
+        </form>
+  
+	</section>
+
+	<footer>
+
+		<h4>Partners Des Devis et Offres Comparatifs</h4>
+
+		<span><strong>Genève - Lausanne - Suisse - CH</strong></span><br />
+		<span><strong>Rhône Alpes - Haute-Savoie - FR</strong></span>
+		<div><a href="tel:+41779601029">CONTACTER</a></div>
+		<div>+41 077 960 10 29</div>
+		<a href="mailto:quotation.partners@gmail.com">quotation.partners@gmail.com</a><br />
+		<a class="copyright">copyright partners services devis déménagements transports 2021 tous droits réservés</a><br />
+
+	</footer>
+
+	</body>
+</html>
